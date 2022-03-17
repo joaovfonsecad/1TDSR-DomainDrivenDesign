ed098453ae7691f987706c4ac970ac92224c8569
joaovfonsecad <joaovfonsecad@gmail.com>
2022-03-17 20:43:03 -0300

    Exercício 6 inserido

--- Aula1/src/com/fiap/Ex6.java
+++ Aula1/src/com/fiap/Ex6.java
@@ -1,7 +1,16 @@
 package com.fiap;
 
+import java.util.Scanner;
+
 public class Ex6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Valor da cotação do dólar: ");
+        double cota = sc.nextDouble();
+        System.out.println("Quantia de dólares a ser convertida: ");
+        double dolls = sc.nextDouble();
+        double reais = dolls * cota;
+        System.out.println("US$ " + dolls + " = R$ " + String.format("%.2f", reais));
     }
 }
