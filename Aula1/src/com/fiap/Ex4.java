88555ba34382dd441aba0b4cb6f7a96d6429bd12
joaovfonsecad <joaovfonsecad@gmail.com>
2022-03-17 20:29:04 -0300

    Exercício 4 adicionado

--- Aula1/src/com/fiap/Ex4.java
+++ Aula1/src/com/fiap/Ex4.java
@@ -1,8 +1,19 @@
 package com.fiap;
 
+import java.util.Scanner;
+
 public class Ex4 {
     public static void main(String[] args) {
-        double a, b, c, d;
-
+        Scanner sc = new Scanner(System.in);
+        int quant = 4;
+        double[] a = new double[quant];
+        double soma = 0;
+        for (int i = 0; i < quant; i++) {
+            System.out.println("Valor " + (i + 1) + ": ");
+            a[i] = sc.nextDouble();
+            soma += a[i];
+        }
+        double media = soma / quant;
+        System.out.println("Média = " + String.format("%.2f", media));
     }
 }
