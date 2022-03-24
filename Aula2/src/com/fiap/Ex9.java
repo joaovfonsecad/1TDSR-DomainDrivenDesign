7f3ac80c30d059fa0ee220d4c28c94c30c9b8b69
joaovfonsecad <joaovfonsecad@gmail.com>
2022-03-24 19:57:20 -0300

    Exercício 9 adicionado

---
+++ Aula2/src/com/fiap/Ex9.java
@@ -0,0 +1,27 @@
+/*
+Entrar via teclado, com dois valores distintos. Exibir o menor deles.
+ */
+
+package com.fiap;
+
+import java.util.Scanner;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Primeiro valor: ");
+        double a = sc.nextDouble();
+        System.out.println("Segundo valor: ");
+        double b = sc.nextDouble();
+
+        if (a < b) {
+            System.out.println("O primeiro valor (" + a + ") é o menor.");
+        }
+        else if (a == b) {
+            System.out.println("Os dois valores são iguais");
+        }
+        else {
+            System.out.println("O segundo valor (" + b + ") é o menor.");
+        }
+    }
+}
